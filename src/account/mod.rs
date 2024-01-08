d578b25003bafdcc1d3d008d8b68e652b29c3a9f
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2024-01-08 17:14:59 +0530

    feat: add initial boilerplate for handling accounts

--- src/account/mod.rs
+++ src/account/mod.rs
@@ -1,5 +1,45 @@
-use crate::Account;
+use crate::{utils::get_network, Account};
 
-pub async fn handler(_rpc_url: String, _account: Account) {
-    // TODO
+use prettytable::{row, Table};
+use solana_account_decoder::UiAccountEncoding;
+use solana_client::{
+    nonblocking::rpc_client::RpcClient as Client, rpc_config::RpcAccountInfoConfig,
+};
+use solana_sdk::account::Account as SolanaAccount;
+
+pub async fn handler(rpc_url: String, account: Account) {
+    // Build RPC client
+    let client = Client::new(get_network(&rpc_url));
+
+    let fetched_account = client
+        .get_account_with_config(
+            &account.pubkey,
+            RpcAccountInfoConfig {
+                encoding: Some(UiAccountEncoding::Base58),
+                ..Default::default()
+            },
+        )
+        .await
+        .unwrap()
+        .value
+        .unwrap();
+
+    let parsed_account = parse_account(fetched_account, &client).await.unwrap();
+
+    parsed_account.view();
+}
+
+async fn parse_account(account: SolanaAccount, client: &Client) -> Option<ParsedAccount> {
+    Some(ParsedAccount {})
+}
+
+pub struct ParsedAccount {}
+
+impl ParsedAccount {
+    fn view(self) {
+        let mut status_table = Table::new();
+        status_table.set_titles(row![
+            c-> "Account Overview",
+        ]);
+    }
 }
